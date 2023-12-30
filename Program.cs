1d9f0cfea549d9b74fb6053181ba0293f680580b
m1a <mag2017go@gmail.com>
2023-12-30 14:02:57 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿namespace examSubtitles
+{
+    class Program
+    {
+        static void Main()
+        {
+            Subtitles subtitles = new Subtitles();
+
+            //вместо работы с файлом и прочее:
+            subtitles.Start.Minutes = int.Parse("02:45".Split(':')[0]);
+            subtitles.Start.Seconds = int.Parse("02:45".Split(':')[1]);
+
+            subtitles.End.Minutes = int.Parse("13:09".Split(':')[0]);
+            subtitles.End.Seconds = int.Parse("13:09".Split(':')[1]);
+
+            subtitles.Location = IsLocation("Top") ? Location.Top : Location.Bottom;
+            subtitles.Color = IsColor("Red") ? Color.Red : Color.White;
+
+            subtitles.Title = "*some input*";
+
+            DisplayText(subtitles);
+        }
+        static void DisplayText(Subtitles subs)
+        {
+            Console.Clear();
+            DrawScreenFrame();
+
+            // Расчет координат для размещения текста в зависимости от указанного местоположения
+            int x, y;
+            Console.WindowWidth = 100;
+            Console.WindowHeight = 30;
+
+            if (subs.Location == Location.Top)
+            {
+                x = Console.WindowWidth / 2 - subs.Title.Length / 2;
+                y = 2;
+            }
+            else if (subs.Location == Location.Bottom)
+            {
+                x = Console.WindowWidth / 2 - subs.Title.Length / 2;
+                y = Console.WindowHeight - 2;
+            }
+            else if (subs.Location == Location.Right)
+            {
+                x = Console.WindowWidth - subs.Title.Length - 2;
+                y = Console.WindowHeight / 2;
+            }
+            else if (subs.Location == Location.Left)
+            {
+                x = 2;
+                y = Console.WindowHeight / 2;
+            }
+            else
+            {
+                x = Console.WindowWidth / 2 - subs.Title.Length / 2;
+                y = Console.WindowHeight / 2;
+            }
+
+            // Установка цвета текста
+            ConsoleColor consoleColor;
+            if (subs.Color == Color.Red)
+            {
+                consoleColor = ConsoleColor.Red;
+            }
+            else if (subs.Color == Color.Green)
+            {
+                consoleColor = ConsoleColor.Green;
+            }
+            else if (subs.Color == Color.Blue)
+            {
+                consoleColor = ConsoleColor.Blue;
+            }
+            else
+            {
+                consoleColor = ConsoleColor.White;
+            }
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(subs.Title);
+
+            // Задержка перед очисткой экрана
+            //int duration = (int.Parse(endTime.Split(':')[1]) - int.Parse(startTime.Split(':')[1])) * 1000;
+            int duration = (subs.Start.Seconds - subs.End.Seconds) * 1000;
+            Thread.Sleep(duration);
+
+            // Очистка экрана
+            Console.Clear();
+            DrawScreenFrame();
+        }
+        static void DrawScreenFrame()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("**************************************************");
+            for (int i = 1; i < Console.WindowHeight - 2; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.WriteLine("*                                                *");
+            }
+            Console.SetCursorPosition(0, Console.WindowHeight - 2);
+            Console.WriteLine("**************************************************");
+        }
+
+        static bool IsColor(string input)
+        {
+            if (input is Color)
+            {
+                return true;
+            }
+            return false;
+        }
+        static bool IsLocation(string input)
+        {
+            if (input is Location)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Subtitles.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examSubtitles
+{
+    class Subtitles
+    {
+        public Time Start { get; set; }
+        public Time End { get; set; }
+        public Location Location { get; set; }
+        public Color Color { get; set; }
+        public string Title { get; set; }
+         
+        public Subtitles()
+        {
+            Start = new Time();
+            End = new Time();
+            Location = Location.Bottom;
+            Color = Color.White;
+            Title = "*empty*";
+        }
+        public Subtitles(Time _start, Time _end, string _title)
+        {
+            Start = _start;
+            End = _end;
+            Title = _title;
+        }
+        public Subtitles(Time _start, Time _end, Location _location, Color _color, string _title)
+        {
+            Start = _start;
+            End = _end;
+            Location = _location;
+            Color = _color;
+            Title = _title;
+        }
+
+        public override string ToString()
+        {
+            return $"{Start} - {End} [{Location}, {Color}] {Title}";    
+        }
+        
+        public static void Print(Subtitles subs)
+        {
+            Console.WriteLine(subs.Start);
+            Console.WriteLine(subs.End);
+            Console.WriteLine(subs.Location);
+            Console.WriteLine(subs.Color);
+            Console.WriteLine(subs.Title);
+        }
+    }
+
+    class Time
+    {
+        //fields
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        //constructor
+        public Time()
+        {
+            Minutes = 0;
+            Seconds = 0;
+        }
+        public Time(int _minutes, int _seconds)
+        {
+            Minutes = _minutes; Seconds = _seconds;
+        }
+
+        public override string ToString()
+        {
+            if (Seconds.ToString().Length == 1 && Minutes.ToString().Length == 1)
+            {
+                return $"0{Minutes}:0{Seconds}";
+            }
+            else if (Seconds.ToString().Length != 1 && Minutes.ToString().Length == 1)
+            {
+                return $"0{Minutes}:{Seconds}";
+            }
+            else if (Seconds.ToString().Length == 1 && Minutes.ToString().Length != 1)
+            {
+                return $"{Minutes}:0{Seconds}";
+            }
+            return $"{Minutes}:{Seconds}";
+            
+        }
+    }
+
+    public enum Color
+    {
+        White,
+        Red,
+        Green,
+        Blue,
+    };
+    public enum Location
+    {
+        Bottom,
+        Top,
+        Right,
+        Left
+    };
+}
